ea4b8d9562ff7733dd528ca4131e0a2f360bb8e3
Arthur Yao <yakunyao@gmail.com>
2020-07-06 10:03:29 +0800

    Finish Phase 3: Tile

--- frontend/tile.jsx
+++ frontend/tile.jsx
@@ -3,12 +3,41 @@ import React from "react";
 export default class Tile extends React.Component {
     constructor(props) {
         super(props);
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(e) {
+        const revealing = e.altKey ? false : true;
+        this.props.updateGame(this, revealing);
     }
 
     render() {
+        let text = "";
+        let klass = "";
+        if (this.props.tile.explored) {
+            if (this.props.tile.flagged) {
+                text = "F";
+                klass = " flagged";
+            } else {
+                if (this.props.bombed) {
+                    text = "B";
+                    klass = " bombed";
+                } else {
+                    const count = this.props.tile.adjacentBombCount();
+                    text = count === 0 ? "" : count;
+                    klass = " revealed";
+                }
+            }
+        } else {
+            text = "";
+        }
+
         return (
-            <div>
-                T
+            <div
+                className={"tile" + klass}
+                onClick={this.handleClick}>
+                {text}
             </div>
         );
     }
